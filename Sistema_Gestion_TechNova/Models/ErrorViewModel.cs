52d6a07b56aceeaf377de20a34698fc0563d839d
santi <santi@DESKTOP-185SQA7>
2025-11-23 18:23:41 -0500

    Agregar archivos de proyecto.

---
+++ Sistema_Gestion_TechNova/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sistema_Gestion_TechNova.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
